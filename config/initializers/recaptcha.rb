b16cdf0531765d054cf815f865952ec07e18a262
nakatyu <carnival_yellow2006@yahoo.co.jp>
2013-01-30 16:33:31 +0900

    設定ファイルのexampleファイルを作成

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6LeZxtoSAAAAAKTiXT5Nk1YjGrNIPkRyI7hhJpk-'
-  config.private_key = '6LeZxtoSAAAAAGxjcukZp_UxE3VtyGinq4D_xFhn'
-  config.proxy = 'inet-gw.adniss.jp:80'
-end
