aeb420168b87d0c16f60f19e0e3a7ecd31cac16b
Masaru Sato <marphy.986@gmail.com>
2013-02-20 13:31:30 +0900

    fix Database Comment.

--- db/migrate/20121206111335_person.rb
+++ db/migrate/20121206111335_person.rb
@@ -2,121 +2,124 @@
 class Person < ActiveRecord::Migration
   def self.up
     create_table :people, :force => true do |t|
-      t.column "person_record_id", :string, :limit => 500
-      t.column "entry_date", :datetime
-      t.column "expiry_date", :datetime
-      t.column "author_name", :string, :limit => 500
-      t.column "author_email", :string, :limit => 500
-      t.column "author_phone", :string, :limit => 500
-      t.column "source_name", :string, :limit => 500
-      t.column "source_date", :datetime
-      t.column "source_url", :string, :limit => 500
-      t.column "full_name", :string, :limit => 500
-      t.column "given_name", :string, :limit => 500
-      t.column "family_name", :string, :limit => 500
-      t.column "alternate_names", :string, :limit => 500
-      t.column "description", :text
-      t.column "sex", :string, :limit => 1
-      t.column "date_of_birth", :date
-      t.column "age", :string, :limit => 500
-      t.column "home_street", :string, :limit => 500
-      t.column "home_neighborhood", :string, :limit => 500
-      t.column "home_city", :string, :limit => 500
-      t.column "home_state", :string, :limit => 500
-      t.column "home_postal_code", :string, :limit => 500
-      t.column "home_country", :string, :limit => 500
-      t.column "photo_url", :string, :limit => 500
-      t.column "profile_urls", :string, :limit => 500
-      t.column "public_flag", :integer
+      t.column "person_record_id",                :string, :limit => 500
+      t.column "entry_date",                      :datetime
+      t.column "expiry_date",                     :datetime
+      t.column "author_name",                     :string, :limit => 500
+      t.column "author_email",                    :string, :limit => 500
+      t.column "author_phone",                    :string, :limit => 500
+      t.column "source_name",                     :string, :limit => 500
+      t.column "source_date",                     :datetime
+      t.column "source_url",                      :string, :limit => 500
+      t.column "full_name",                       :string, :limit => 500
+      t.column "given_name",                      :string, :limit => 500
+      t.column "family_name",                     :string, :limit => 500
+      t.column "alternate_names",                 :string, :limit => 500
+      t.column "description",                     :text
+      t.column "sex",                             :string, :limit => 1
+      t.column "date_of_birth",                   :date
+      t.column "age",                             :string, :limit => 500
+      t.column "home_street",                     :string, :limit => 500
+      t.column "home_neighborhood",               :string, :limit => 500
+      t.column "home_city",                       :string, :limit => 500
+      t.column "home_state",                      :string, :limit => 500
+      t.column "home_postal_code",                :string, :limit => 500
+      t.column "home_country",                    :string, :limit => 500
+      t.column "photo_url",                       :string, :limit => 500
+      t.column "profile_urls",                    :string, :limit => 500
+      t.column "public_flag",                     :integer
 
-      t.column "in_city_flag", :string, :limit => 1
-      t.column "shelter_name", :string, :limit => 20
-      t.column "refuge_status", :string, :limit => 1
-      t.column "refuge_reason", :text
-      t.column "shelter_entry_date", :date
-      t.column "shelter_leave_date", :date
-      t.column "next_place", :string, :limit => 100
-      t.column "next_place_phone", :string, :limit => 20
-      t.column "injury_flag", :string, :limit => 1
-      t.column "injury_condition", :text
-      t.column "allergy_flag", :string, :limit => 1
-      t.column "allergy_cause", :text
-      t.column "pregnancy", :string, :limit => 1
-      t.column "baby", :string, :limit => 1
-      t.column "upper_care_level_three", :string, :limit => 2
-      t.column "elderly_alone", :string, :limit => 1
-      t.column "elderly_couple", :string, :limit => 1
-      t.column "bedridden_elderly", :string, :limit => 1
-      t.column "elderly_dementia", :string, :limit => 1
-      t.column "rehabilitation_certificate", :string, :limit => 2
+      t.column "in_city_flag",                    :string, :limit => 1
+      t.column "shelter_name",                    :string, :limit => 20
+      t.column "refuge_status",                   :string, :limit => 1
+      t.column "refuge_reason",                   :text
+      t.column "shelter_entry_date",              :date
+      t.column "shelter_leave_date",              :date
+      t.column "next_place",                      :string, :limit => 100
+      t.column "next_place_phone",                :string, :limit => 20
+      t.column "injury_flag",                     :string, :limit => 1
+      t.column "injury_condition",                :text
+      t.column "allergy_flag",                    :string, :limit => 1
+      t.column "allergy_cause",                   :text
+      t.column "pregnancy",                       :string, :limit => 1
+      t.column "baby",                            :string, :limit => 1
+      t.column "upper_care_level_three",          :string, :limit => 2
+      t.column "elderly_alone",                   :string, :limit => 1
+      t.column "elderly_couple",                  :string, :limit => 1
+      t.column "bedridden_elderly",               :string, :limit => 1
+      t.column "elderly_dementia",                :string, :limit => 1
+      t.column "rehabilitation_certificate",      :string, :limit => 2
       t.column "physical_disability_certificate", :string, :limit => 1
-      t.column "family_well", :string, :limit => 1
-      t.column "link_flag", :boolean, :default => false
-      t.column "notes_disabled", :boolean, :default => false
-      t.column "email_flag", :boolean, :default => false
-      t.column "deleted_at", :datetime
+      t.column "family_well",                     :string, :limit => 1
+      t.column "link_flag",                       :boolean, :default => false
+      t.column "notes_disabled",                  :boolean, :default => false
+      t.column "email_flag",                      :boolean, :default => false
+      t.column "deleted_at",                      :datetime
+
       t.timestamps
     end
 
     change_table :people do |t|
-      t.set_column_comment "person_record_id", "GooglePersonFinderのperson_id"
-      t.set_column_comment "entry_date", "レコード作成日時"
-      t.set_column_comment "expiry_date", "レコード削除予定日時"
-      t.set_column_comment "author_name", "レコード作成者名"
-      t.set_column_comment "author_email", "レコード作成者のメールアドレス"
-      t.set_column_comment "author_phone", "レコード作成者の電話番号"
-      t.set_column_comment "source_name", "情報元のサイト名"
-      t.set_column_comment "source_date", "情報元の投稿日"
-      t.set_column_comment "source_url", "情報元のサイトURL"
-      t.set_column_comment "full_name", "避難者名前"
-      t.set_column_comment "given_name", "避難者_名"
-      t.set_column_comment "family_name", "避難者_姓"
-      t.set_column_comment "alternate_names", "避難者_よみがな"
-      t.set_column_comment "description", "説明"
-      t.set_column_comment "sex", "性別"
-      t.set_column_comment "date_of_birth", "生年月日"
-      t.set_column_comment "age", "年齢"
-      t.set_column_comment "home_street", "町名"
-      t.set_column_comment "home_neighborhood", "近隣"
-      t.set_column_comment "home_city", "市区町村"
-      t.set_column_comment "home_state", "都道府県"
-      t.set_column_comment "home_postal_code", "郵便番号"
-      t.set_column_comment "home_country", "出身国"
-      t.set_column_comment "photo_url", "写真のURL"
-      t.set_column_comment "profile_urls", "プロフィールURL"
-      t.set_column_comment "public_flag", '公開フラグ'
+      t.set_column_comment "id",                     "このレコードのプライマリ ID"
+      
+      t.set_column_comment "person_record_id",       "この対象者情報の一意の ID（RFC4122 UUID）"
+      t.set_column_comment "entry_date",             "この対象者情報の作成日時"
+      t.set_column_comment "expiry_date",            "この対象者情報の削除予定日時"
+      t.set_column_comment "author_name",            "この対象者情報の作成者名"
+      t.set_column_comment "author_email",           "この対象者情報の作成者の電子メールアドレス"
+      t.set_column_comment "author_phone",           "この対象者情報の作成者の電話番号"
+      t.set_column_comment "source_name",            "この対象者情報の情報元のサイト名"
+      t.set_column_comment "source_date",            "この対象者情報の情報元における対象者情報の投稿日"
+      t.set_column_comment "source_url",             "この対象者情報の作成元の対象者情報への URL"
+      t.set_column_comment "full_name",              "対象者の氏名"
+      t.set_column_comment "given_name",             "対象者の氏名（名）"
+      t.set_column_comment "family_name",            "対象者の氏名（姓）"
+      t.set_column_comment "alternate_names",        "対象者の氏名（ふりがな）"
+      t.set_column_comment "description",            "対象者の詳細な特徴／説明"
+      t.set_column_comment "sex",                    "対象者の性別"
+      t.set_column_comment "date_of_birth",          "対象者の生年月日"
+      t.set_column_comment "age",                    "対象者の年齢"
+      t.set_column_comment "home_street",            "対象者の住所（町名）"
+      t.set_column_comment "home_neighborhood",      "対象者の住所（近隣の場所）"
+      t.set_column_comment "home_city",              "対象者の住所（市区町村名）"
+      t.set_column_comment "home_state",             "対象者の住所（都道府県名）"
+      t.set_column_comment "home_postal_code",       "対象者の住所（郵便番号）"
+      t.set_column_comment "home_country",           "対象者の出身国"
+      t.set_column_comment "photo_url",              "対象者の写真の URL"
+      t.set_column_comment "profile_urls",           "対象者のプロフィール URL"
+      t.set_column_comment "public_flag",            "公開フラグ"
 
-      t.set_column_comment "in_city_flag", "市内・市外区分"
-      t.set_column_comment "shelter_name", "避難所"
-      t.set_column_comment "refuge_status", "避難状況"
-      t.set_column_comment "refuge_reason", "避難理由"
-      t.set_column_comment "shelter_entry_date", "入所年月日"
-      t.set_column_comment "shelter_leave_date", "退所年月日"
-      t.set_column_comment "next_place", "退所先"
-      t.set_column_comment "next_place_phone", "退所先電話番号"
-      t.set_column_comment "injury_flag", "負傷"
-      t.set_column_comment "injury_condition", "負傷内容"
-      t.set_column_comment "allergy_flag", "アレルギー"
-      t.set_column_comment "allergy_cause", "アレルギー物質"
-      t.set_column_comment "pregnancy", "妊婦"
-      t.set_column_comment "baby", "乳幼児"
-      t.set_column_comment "upper_care_level_three", "要介護度３以上"
-      t.set_column_comment "elderly_alone", "１人暮らしの高齢者"
-      t.set_column_comment "elderly_couple", "高齢者世帯"
-      t.set_column_comment "bedridden_elderly", "寝たきり高齢者"
-      t.set_column_comment "elderly_dementia", "認知症高齢者"
-      t.set_column_comment "rehabilitation_certificate", " 療育手帳所持者"
-      t.set_column_comment "physical_disability_certificate", "身体障害者手帳所持者"
-      t.set_column_comment "family_well", "家族も無事"
-      t.set_column_comment "link_flag", "連携フラグ"
-      t.set_column_comment "notes_disabled", "メモ無効フラグ"
-      t.set_column_comment "email_flag", "新着メッセージ受取フラグ"
-      t.set_column_comment "deleted_at", "削除日時"
-      t.set_column_comment "created_at", "登録日時"
-      t.set_column_comment "updated_at", "更新日時"
-    end                    
-  end                      
-                           
-  def down                 
-  end                      
-end                        
+      t.set_column_comment "in_city_flag",           "対象者が LGDPF を運用する市区町村の住民である場合を True とするフラグ"
+      t.set_column_comment "shelter_name",           "対象者が避難している／した避難所名"
+      t.set_column_comment "refuge_status",          "対象者の「避難状況区分」"
+      t.set_column_comment "refuge_reason",          "対象者が避難した理由"
+      t.set_column_comment "shelter_entry_date",     "対象者が避難所に避難した場合の入所年月日"
+      t.set_column_comment "shelter_leave_date",     "対象者が避難所に避難した場合の退所年月日"
+      t.set_column_comment "next_place",             "対象者が避難所を退所した後の行き先"
+      t.set_column_comment "next_place_phone",       "対象者が避難所を退所した後の電話番号"
+      t.set_column_comment "injury_flag",            "対象者が災害により負傷している場合を True とするフラグ"
+      t.set_column_comment "injury_condition",       "対象者が負傷している場合の具体的な内容"
+      t.set_column_comment "allergy_flag",           "対象者がアレルギー保持者である場合を True とするフラグ"
+      t.set_column_comment "allergy_cause",          "対象者がアレルギー保持者である場合の具体的な内容"
+      t.set_column_comment "pregnancy",              "対象者が「妊婦」に該当する場合を True とするフラグ"
+      t.set_column_comment "baby",                   "対象者が該当する「乳幼児区分」"
+      t.set_column_comment "upper_care_level_three", "対象者が該当する「要介護度区分」"
+      t.set_column_comment "elderly_alone",          "対象者が「６５歳以上の１人暮らしの高齢者」に該当する場合を True とするフラグ"
+      t.set_column_comment "elderly_couple",         "対象者が「６５歳以上の高齢者のみ世帯」に該当する場合を True とするフラグ"
+      t.set_column_comment "bedridden_elderly",      "対象者が「寝たきり高齢者」に該当する場合を True とするフラグ"
+      t.set_column_comment "elderly_dementia",       "対象者が「認知症高齢者」に該当する場合を True とするフラグ"
+      t.set_column_comment "rehabilitation_certificate", "対象者が該当する「療育手帳障害区分」"
+      t.set_column_comment "physical_disability_certificate", "対象者が該当する「身体障害者手帳障害区分」"
+      t.set_column_comment "family_well",            "対象者の家族について「家族全員の無事を確認した」場合を True とするフラグ"
+      t.set_column_comment "link_flag",              "この対象者情報を LGDPF/LGDPM 以外の安否確認システムとの間で連携することを、対象者情報の作成者が許容する場合を True とするフラグ"
+      t.set_column_comment "notes_disabled",         "メモ無効フラグ"
+      t.set_column_comment "email_flag",             "この対象者に関連する情報に変化が生じた場合にメッセージを送る事を、対象者情報の作成者が希望する場合を True とするフラグ"
+      t.set_column_comment "deleted_at",             "この対象者情報の削除日時"
+      t.set_column_comment "created_at",             "このレコードの作成日時"
+      t.set_column_comment "updated_at",             "このレコードの更新日時"
+    end
+  end
+            
+  def down
+  end
+end
--- db/migrate/20121206111400_note.rb
+++ db/migrate/20121206111400_note.rb
@@ -2,46 +2,51 @@
 class Note < ActiveRecord::Migration
   def up
     create_table :notes, :force => true do |t|
-      t.column "note_record_id", :string, :limit => 500
-      t.column "person_record_id", :integer
+      t.column "note_record_id",          :string, :limit => 500
+      t.column "person_record_id",        :integer
       t.column "linked_person_record_id", :string, :limit => 500
-      t.column "entry_date", :datetime
-      t.column "author_name", :string, :limit => 500
-      t.column "author_email", :string, :limit => 500
-      t.column "author_phone", :string, :limit => 500
-      t.column "source_date", :datetime
-      t.column "author_made_contact", :boolean, :default => false
-      t.column "status", :integer
-      t.column "email_of_found_person", :string, :limit => 500
-      t.column "phone_of_found_person", :string, :limit => 500
-      t.column "last_known_location", :string, :limit => 500
-      t.column "text", :text
-      t.column "photo_url", :string, :limit => 500
-      t.column "spam_flag", :boolean
-      t.column "link_flag", :boolean, :default => false
-      t.column "email_flag", :boolean, :default => false
+      t.column "entry_date",              :datetime
+      t.column "author_name",             :string, :limit => 500
+      t.column "author_email",            :string, :limit => 500
+      t.column "author_phone",            :string, :limit => 500
+      t.column "source_date",             :datetime
+      t.column "author_made_contact",     :boolean, :default => false
+      t.column "status",                  :integer
+      t.column "email_of_found_person",   :string, :limit => 500
+      t.column "phone_of_found_person",   :string, :limit => 500
+      t.column "last_known_location",     :string, :limit => 500
+      t.column "text",                    :text
+      t.column "photo_url",               :string, :limit => 500
+      t.column "spam_flag",               :boolean
+      t.column "link_flag",               :boolean, :default => false
+      t.column "email_flag",              :boolean, :default => false
       t.timestamps
     end
     
     change_table :notes do |t|
-      t.set_column_comment :note_record_id, 'GooglePersonFinderのnote_id'
-      t.set_column_comment :person_record_id, '避難者情報との外部キー'
-      t.set_column_comment :linked_person_record_id, '重複キー'
-      t.set_column_comment :entry_date, '登録日'
-      t.set_column_comment :author_name, '投稿者'
-      t.set_column_comment :author_email, '投稿者のメールアドレス'
-      t.set_column_comment :author_phone, '投稿者の電話番号'
-      t.set_column_comment :source_date, '作成日時'
-      t.set_column_comment :author_made_contact, '連絡の有無'
-      t.set_column_comment :status, '状況'
-      t.set_column_comment :email_of_found_person, '避難者のメールアドレス'
-      t.set_column_comment :phone_of_found_person, '避難者の電話番号'
-      t.set_column_comment :last_known_location, '最後に見かけた場所'
-      t.set_column_comment :text, 'メッセージ'
-      t.set_column_comment :photo_url, '写真のURL'
-      t.set_column_comment :spam_flag, 'スパムフラグ'
-      t.set_column_comment :link_flag, '連携フラグ'
-      t.set_column_comment :email_flag, "新着メッセージ受取フラグ"
+      t.set_column_comment :id,                      'このレコードのプライマリ ID'
+
+      t.set_column_comment :note_record_id,          'このメモ情報の一意の ID（RFC4122 UUID）'
+      t.set_column_comment :person_record_id,        'このメモ情報が紐づく対象者情報への外部キー'
+      t.set_column_comment :linked_person_record_id, 'このメモ情報が紐づく重複した対象者情報への外部キー'
+      t.set_column_comment :entry_date,              'このメモ情報の作成日時'
+      t.set_column_comment :author_name,             'このメモ情報の作成者名'
+      t.set_column_comment :author_email,            'このメモ情報の作成者の電子メールアドレス'
+      t.set_column_comment :author_phone,            'このメモ情報の作成者の電話番号'
+      t.set_column_comment :source_date,             'このメモ情報の情報元におけるメモ情報の投稿日'
+      t.set_column_comment :author_made_contact,     'このメモ情報の作成者がこのメモ情報が紐づく対象者と連絡がとれた場合を True とするフラグ'
+      t.set_column_comment :status,                  'このメモが表す状況区分'
+      t.set_column_comment :email_of_found_person,   'このメモ情報が紐づく対象者の現在の電子メールアドレス'
+      t.set_column_comment :phone_of_found_person,   'このメモ情報が紐づく対象者の現在の電話番号'
+      t.set_column_comment :last_known_location,     'このメモ情報が紐づく対象者を最後に見かけた場所'
+      t.set_column_comment :text,                    'このメモのメッセージ'
+      t.set_column_comment :photo_url,               'このメモに関連する写真への URL'
+      t.set_column_comment :spam_flag,               'このメモ情報がスパムである場合を True とするフラグ'
+      t.set_column_comment :link_flag,               'このメモ情報を LGDPF/LGDPM 以外の安否確認システムとの間で連携することを、メモ情報の作成者が許容する場合を True とするフラグ'
+      t.set_column_comment :email_flag,              'このメモに関連する情報に変化が生じた場合にメッセージを送る事を、メモ情報の作成者が希望する場合を True とするフラグ'
+      
+      t.set_column_comment :created_at,              'このレコードの作成日時'
+      t.set_column_comment :updated_at,              'このレコードの更新日時'
     end
   end
 
