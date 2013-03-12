260f0a25a1e28db084b21ad92fb94979b48523f2
nakatyu <carnival_yellow2006@yahoo.co.jp>
2013-03-12 18:06:10 +0900

    指摘点修正
    避難所の開設状況が「開設」と「常設」のものを抽出する

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,13 +2,21 @@
 class Shelter < ActiveRecord::Base
   attr_accessible :name, :name_kana, :area, :address, :shelter_type, :shelter_sort, :shelter_code
 
+  # 定数
+  # 開設状況
+  SHELTER_SORT_MIKAISETSU = "1"  # 未開設
+  SHELTER_SORT_KAISETSU   = "2"  # 開設
+  SHELTER_SORT_HEISA      = "3"  # 閉鎖
+  SHELTER_SORT_FUMEI      = "4"  # 不明
+  SHELTER_SORT_JOSETSU    = "5"  # 常設
+
   # コンスタントマスタからデータを取得する
   # === Args
   # === Return
   # Shelterハッシュ
   def self.hash_for_selectbox
     shelter_list = {}
-    shelters = Shelter.all
+    shelters = Shelter.where(:shelter_sort => [SHELTER_SORT_KAISETSU, SHELTER_SORT_JOSETSU])
     shelters.each do |s|
       shelter_list[s.shelter_code] = s.name
     end
