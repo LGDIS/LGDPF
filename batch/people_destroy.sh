af4f307de0de4ab2c3d19e9561796c3109e53701
nakatyu <carnival_yellow2006@yahoo.co.jp>
2013-03-14 17:02:54 +0900

    コメント修正

--- batch/people_destroy.sh
+++ batch/people_destroy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #有効期限の切れた避難者情報を論理削除する
 
-PATH=$PATH:/usr/local/bin
-export PATH
+# Rails.rootに遷移する
+cd /path/to/deploy
 
-#cd /path/to/deploy
-cd /home/apl/develop/lgdpf/LGDPF/batch
-
-rails runner Batches::PeopleDestroy.execute -e production >> ../log/people_destroy.log 2>&1
+rails runner Batches::PeopleDestroy.execute -e production >> log/people_destroy.log 2>&1
 
 exit 0
--- batch/synchro_with_google_person_finder.sh
+++ batch/synchro_with_google_person_finder.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-PATH=$PATH:/usr/local/bin
-export PATH
-
+# Rails.rootに遷移する
 cd /path/to/deploy
 
 #GooglePersonFinderから「石巻市」のデータを取り込む
-rails runner Batches::ImportGooglePersonFinder.execute -e production >> ../log/import_google_person_finder.log 2>&1
+rails runner Batches::ImportGooglePersonFinder.execute -e production >> log/import_google_person_finder.log 2>&1
 
 #GooglePersonFinderへデータをアップロードする
-rails runner Batches::ExportGooglePersonFinder.execute -e production >> ../log/export_google_person_finder.log 2>&1
+rails runner Batches::ExportGooglePersonFinder.execute -e production >> log/export_google_person_finder.log 2>&1
 
 exit 0
