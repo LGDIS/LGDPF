4f436c03c3dc4b45644d4a0bd9cb26f9c06d766d
tyanagisawa <Yanagisawa.Toshihumi@adniss.jp>
2013-04-15 14:25:44 +0900

    画像ファイルアップロード時のエラーメッセージを日本語で表示する(前回対応時の修正漏れ)

--- config/initializers/carrierwave_override.rb
+++ config/initializers/carrierwave_override.rb
@@ -4,17 +4,21 @@ module CarrierWave
   module Uploader
     module Download
       class RemoteFile
-        # 特異メソッド
-        # エラーメッセージを日本語にする
-        def file_with_japanize
-          file_without_japanize
-        rescue Exception => e
+        private
+        def file_with_i18n
+          file_without_i18n
+        rescue CarrierWave::DownloadError
           raise CarrierWave::DownloadError,  I18n.t("activerecord.errors.messages.record_invalid")
         end
-
-        alias_method_chain :file, :japanize
-        
+        alias_method_chain :file, :i18n
       end
+
+      def download_with_i18n!(uri)
+        download_without_i18n!(uri)
+      rescue CarrierWave::DownloadError
+        raise CarrierWave::DownloadError,  I18n.t("activerecord.errors.messages.record_invalid")
+      end
+      alias_method_chain :download!, :i18n
     end
   end
 end
